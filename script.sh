05eb86013dbb41e3a68b4a7cd3c38384a0d14fcc
Egor Beluga <egorbeluga@gmail.com>
2022-05-29 09:44:56 +0300

    make changes

--- script.sh
+++ script.sh
@@ -1,34 +1,27 @@
 #! /bin/bash/
 
 # зайти в папку
-
 cd $HOME;
 
 # создать 3 папки 
-
 mkdir HW1Beluga;
 cd $HOME/HW1Beluga/;
 mkdir 1 2 3;
 
 # зайти в любую папку
-
 cd $HOME/HW1Beluga/1;
 
 # создать 5 файлов (3 txt, 2 json)
-
 touch new-file-{1..3}.txt
 touch new-file-{1..2}.json
 
 # создать 3 папки
-
 mkdir 1.1 1.2 1.3;
 
 # вывести создержимое папки
-
 ls
 
 # переместить любые два файла , которые вы создали в другую папку 
-
 mv new-file-1.txt $HOME/HW1Beluga/1/1.1/;
 cd $HOME/HW1Beluga/1/;
 ls
